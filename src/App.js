a49576e2477275084aed4e6f8a42734d9e41ee4a
vet <vetefir@gmail.com>
2023-04-04 17:31:26 +0200

    commit before the change

--- src/App.js
+++ src/App.js
@@ -31,47 +31,36 @@ function NavCategory() {
 
 	useEffect(() => {
 
-/* 
-	To tell the development server to proxy any unknown requests to your API server in development, 
-	add a proxy field to your package.json, for example:
-	"proxy": "http://localhost:8001",   - 
-	This way, when you fetch('/api/category') in development, the development server will recognize 
-	that it’s not a static asset, and will proxy your request to http://localhost:8001/api/category as a fallback. 
-	The development server will only attempt to send requests without text/html in its Accept header to the proxy.
-	Conveniently, this avoids CORS issues and error messages in development
-	https://create-react-app.dev/docs/proxying-api-requests-in-development/
-*/
 
+	/* 
+		To tell the development server to proxy any unknown requests to your API server in development, 
+		add a proxy field to your package.json, for example:
+		"proxy": "http://localhost:8001",   - 
+		This way, when you fetch('/api/category') in development, the development server will recognize 
+		that it’s not a static asset, and will proxy your request to http://localhost:8001/api/category as a fallback. 
+		The development server will only attempt to send requests without text/html in its Accept header to the proxy.
+		Conveniently, this avoids CORS issues and error messages in development
+		https://create-react-app.dev/docs/proxying-api-requests-in-development/
+	*/
 
-		fetch('/categs')
-			.then((res) => res.json())
-			.then((data) => {
-				console.log('data:', data.data);
-				setCategories(data.data);
-				// setData(data.message)
-			});
+	fetch('/categs')
+		.then((res) => res.json())  		// res.json() -function returns an Object
+		.then((dataObj) => {
+			console.log('dataObj:', dataObj, ' | ', dataObj.data);	// {data: Array(4)} | Array(4)
+			setCategories(dataObj.data);										// Array(4)
+			// setData(data.message)
+	});
 
-/* 
+	/* 
 		axios
 		.get("https://localhost:8001")
 		.then((response) => {
-		  console.log(response);
-		  // setItems(response.data);
+		console.log(response);
+		// setItems(response.data);
 		})
 		.catch((error) => console.log(error));
-
- */		
-/* 
-		categories.get("/all", async (req, res) => {
-			console.log(pool);
-			try {
-			  const { rows } = await pool.query("select * from category;");
-			  res.json(rows);
-			} catch (err) {
-			  res.sendStatus(500);
-			}
-		 });
- */		
+	*/		
+	
 	}, []);
 
 	return (
@@ -90,27 +79,6 @@ function NavCategory() {
 		</ul>
 		</nav>
 	);
-
-  /*
-	// version2: onClick - category Name für Filter übergeben 
-
-	const resipeFiltr = (categ) => {
-		console.log(categ);
-	}
-
-	return (
-	  <div className="Category">
-		 <h2>Category</h2>
-		 <ul>
-			{categories.map((category, idx) => (
-				<li key={idx} onClick={() => resipeFiltr(category.fields.cateName)}>
-						<h3>{category.fields.cateName}</h3>
-				</li>
-		 ))}
-		 </ul>
-	  </div>
-	);
- */
 }
 
 export default App;
